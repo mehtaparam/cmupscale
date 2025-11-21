6fb95469d2aff41d60f403313ce8f8bbb77f250b
Param Mehta <mehtaparam@ymail.com>
2025-11-21 14:49:46 +0530

    updated code

--- dist/routes/upscaler.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var upscaler_controller_1 = require("../controllers/upscaler.controller");
-var router = (0, express_1.Router)();
-router.post('/upscale', upscaler_controller_1.upscaleImage);
-exports.default = router;
-//# sourceMappingURL=upscaler.routes.js.map
